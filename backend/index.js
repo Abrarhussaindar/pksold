3bf8a178689a4e981ab2e8f76ed8767dc414c57b
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2023-09-09 15:53:39 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+
+
+// config
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
